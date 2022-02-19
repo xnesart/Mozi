2f129c906c90d5abeb43c15e302062030b01c732
xnesart <60525875+xnesart@users.noreply.github.com>
2022-02-19 22:53:57 +0300

    решаю вопрос со слайдером

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,4 +3,42 @@ import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
-
+$(document).ready(function () {
+  $(".slider-waves__list").slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow: $(".slider-waves__arrow-right"),
+  nextArrow: $(".slider-waves__arrow-left"),
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+  });
+});
